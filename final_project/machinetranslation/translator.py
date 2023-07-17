b9a171b08220241d76b76b6a850c5bb056fc547a
Jason <jasonfong12398@gmail.com>
2023-07-17 14:24:01 -0400

    fixed language translator

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -5,11 +5,11 @@ from deep_translator import MyMemoryTranslator
 def english_to_french(english_text):
     """ Translates English to French
     """
-    french_text = MyMemoryTranslator(source = "en-US", target = "fr-CA").translate(english_text)
+    french_text = MyMemoryTranslator(source = "en", target = "fr").translate(english_text)
     return french_text
 
 def french_to_english(french_text):
     """ Translates French to English
     """
-    english_text = MyMemoryTranslator(source = "fr-CA", target = "en-US").translate(french_text)
+    english_text = MyMemoryTranslator(source = "fr", target = "en").translate(french_text)
     return english_text
